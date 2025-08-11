5fdc5f9af15968955499549db0d2af37e69b1fe1
mariam4132 <mariam.fatimaa007@gmail.com>
2025-08-11 23:08:19 +0400

    updated server file

--- config/server.js
+++ config/server.js
@@ -1,13 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-
-  // Public URL for production (important for admin links, email, etc.)
-  url: env("PUBLIC_URL", "http://localhost:1337"),
+  url: env("PUBLIC_URL"), // no default to localhost here!
   app: {
     keys: env.array("APP_KEYS"),
   },
-
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
